2f83fb111476d5d1b17d762c7d1c96a32479b27f
Ben Schembri <benschembri@gmail.com>
2025-09-27 17:34:05 +1000

    setup: add prettier packages and config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,14 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import typescriptPlugin from '@typescript-eslint/eslint-plugin'
-import reactPlugin from 'eslint-plugin-react'
-import reactHooksPlugin from 'eslint-plugin-react-hooks'
-import importPlugin from 'eslint-plugin-import'
-import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
+import { FlatCompat } from '@eslint/eslintrc';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const compat = new FlatCompat({
   baseDirectory: process.cwd(),
-})
+});
 
 export default [
   ...compat.extends('airbnb'),
@@ -29,10 +30,12 @@ export default [
       'react-hooks': reactHooksPlugin,
       import: importPlugin,
       'jsx-a11y': jsxA11yPlugin,
+      prettier: prettierPlugin,
     },
     rules: {
       'react/react-in-jsx-scope': 'off', // Suppress 'React must be in scope errors when using React 17+
       'react/jsx-uses-react': 'off', // Suppress 'React must be in scope errors when using React 17+
+      'prettier/prettier': 'error', // Formatting issues become ESLint errors
       // Add any custom rules here
     },
     settings: {
@@ -40,4 +43,4 @@ export default [
       'import/resolver': { typescript: {} },
     },
   },
-]
+];
