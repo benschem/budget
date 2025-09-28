64f3e826a98a1621d2ea16372a2dc8e021701346
Ben Schembri <benschembri@gmail.com>
2025-09-28 17:09:41 +1000

    feat: data models

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,54 @@
-export type Category = {
-  id: string;
-  name: string;
-  annualBudget: number;
-  monthlySpends: { [month: string]: number }; // { "2025-01": 500, "2025-02": 420 }
-};
-
-export type SpendingEntry = {
-  categoryId: string;
-  month: string;
+export interface Income {
+  id: number;
   amount: number;
-};
+  date: string; // ISO date string
+  source: string;
+  notes?: string;
+}
 
-export type BudgetData = {
+export interface Expense {
+  id: number;
+  amount: number;
+  date: string; // ISO date string
+  category_id: number;
+  notes?: string;
+}
+
+export interface ProjectedIncome {
+  id: number;
+  amount: number;
+  date: string; // ISO date string
+  source: string;
+  notes?: string;
+  recurrence: 'once' | 'weekly' | 'fortnightly' | 'monthly' | 'annually'
+}
+
+export interface ProjectedExpense {
+  id: number;
+  amount: number;
+  expected_date: string; // ISO date string
+  category_id: number;
+  notes?: string;
+  recurrence: 'once' | 'weekly' | 'fortnightly' | 'monthly' | 'annually'
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Money {
+  id: number;
+  amount: number;
+  category_id?: number; // if undefined = "free money"
+}
+
+export interface BudgetData {
+  starting_balance: number; // the amount already in the bank at the time you start tracking
+  income: Income[];
+  expenses: Expense[];
+  projected_income: ProjectedIncome[];
+  projected_expenses: ProjectedExpense[];
   categories: Category[];
-};
+  money: Money[];
+}
